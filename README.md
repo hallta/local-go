bf79371c1ea9cfeb6016dfe5449099f17d5c1f42
Trevor Hall <hallta@gmail.com>
2024-02-15 12:23:37 -0800

    new little go service

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # local-go
+
 Mini redirector service
+
+# Run
+
+1. `python3 -m venv env`
+1. `source env/bin/activate`
+1. `flask --app amin run`
+
+# Configiure
+
+1. `127.0.0.1/go/<url>` will work
+1. Configure a Chrome search to "go"
+1. Save new urls either at the `config` file or at `/save?p=%s&u=%s`
---
+++ main.py
@@ -0,0 +1,43 @@
+from flask import Flask, abort,request, redirect
+
+app = Flask(__name__)
+redir_map = {}
+
+@app.errorhandler(404)
+def not_found(e):
+    return "URL not found"
+
+
+@app.route("/go/<string:path>")
+def go(path):
+    if path in redir_map:
+        return redirect("https://" + redir_map[path], code=302)
+    else:
+        abort(404)
+
+
+@app.route('/save')
+def save():
+    path = request.args.get('p')
+    url = request.args.get('u')
+    redir_map[path] = url
+
+    config = open('config', 'a')
+    config.write(path + ':' + url + "\n")
+    config.close()
+
+    return "👍"
+
+
+
+def load_config():
+    with open("config", "r") as config:
+        for option in config:
+            path, url = option.split(':', 1)
+            redir_map[path] = url.rstrip()
+
+
+def startup():
+    load_config()
+
+startup()
